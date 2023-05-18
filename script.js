e85a5752d70f810e2e1fc9b299970d2ad588c7dd
MedinaZS <101607623+MedinaZS@users.noreply.github.com>
2023-05-18 10:16:23 -0400

    Add/modify icons sidebar

--- script.js
+++ script.js
@@ -109,3 +109,25 @@ let addBackground = async (data) => {
     return txt;
 }
 
+
+
+let navOpen = true;
+
+document.querySelector("#btn-sidebar").addEventListener("click",() => {
+    (navOpen) ? closeNav() : openNav() 
+})
+
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    navOpen = true;
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("sidebar").style.width = "";
+    document.getElementById("main").style.marginLeft = "0";
+    navOpen = false;
+}
